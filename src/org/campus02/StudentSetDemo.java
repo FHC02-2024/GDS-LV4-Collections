6dd384a0b5a6011b1767d1acefc663a8e096c139
Daniel Zettel <daniel.zettel@lv.campus02.at>
2024-10-19 13:55:39 +0200

    StudentSetDemo finished

--- src/org/campus02/StudentSetDemo.java
+++ src/org/campus02/StudentSetDemo.java
@@ -1,4 +1,50 @@
 package org.campus02;
 
+import java.util.HashSet;
+
 public class StudentSetDemo {
+
+    public static void main(String[] args) {
+        HashSet<Student> students = new HashSet<>();
+        Student max = new Student(1, "max");
+        Student susi = new Student(2, "susi");
+
+        students.add(max);
+        students.add(susi);
+        students.add(new Student(3, "john"));
+
+        // duplikate nicht erlaubt
+        students.add(max);
+
+        for (Student s : students) {
+            System.out.println(s);
+            System.out.println(s.hashCode());
+        }
+
+
+        System.out.println();
+        students.remove(susi);
+        for (Student s : students) {
+            System.out.println(s);
+            System.out.println(s.hashCode());
+        }
+
+        Student max2 = new Student(4, "max");
+        students.add(max2);
+
+        System.out.println();
+        for (Student s : students) {
+            System.out.println(s);
+            System.out.println(s.hashCode());
+        }
+
+        Student maria = new Student(1, "maria");
+        students.add(maria);
+
+        System.out.println();
+        for (Student s : students) {
+            System.out.println(s);
+            System.out.println(s.hashCode());
+        }
+    }
 }
